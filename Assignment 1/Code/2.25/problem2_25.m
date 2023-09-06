31ac3cf34a56027319c83497afc517b82f7f0d12
Kymkim <82325590+Kymkim@users.noreply.github.com>
2023-09-05 20:06:38 -0700

    initial commit for problem 2.25

---
+++ Assignment 1/Code/2.25/problem2_25.m
@@ -0,0 +1,18 @@
+%{
+Name: Assignment 1: Problem 2.25
+Date: 09/05/2023
+Class: CECS 271
+Instructor: Minhthong Nguyen
+Purpose: A program that displays the interest rate, amount of interest,
+balance of a savings account given in exercise  2.25 in Essential MATLABfor
+Engineers and Scientists (6th Edition)
+Last updated: 09/05/2023
+%}
+
+%Initial variables and settings
+format bank
+bal = 100000;
+    
+for month = [1:1:12]
+
+
