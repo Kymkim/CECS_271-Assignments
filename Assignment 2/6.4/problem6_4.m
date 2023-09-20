e60df0e79f5bd1aeb2f88d1ceeb4bdcd71a239c2
Kymkim <82325590+Kymkim@users.noreply.github.com>
2023-09-20 08:46:33 -0700

    finished 6.4

--- Assignment 2/6.4/problem6_4.m
+++ Assignment 2/6.4/problem6_4.m
@@ -8,9 +8,6 @@ conditioning
 Last updated: 09/05/2023
 %}
 
-%PART A
-disp("Part A of Question 6.4")
-
 %Using left division to show x = 2 and y = 3.
 A = [1 5.000; 1.5 7.501];
 B = [17.0; 25.503];
@@ -21,17 +18,59 @@ disp(['Solution for the system of A and B x = ', num2str(x(1)), ' y = ', num2str
 % Residual = coefficient*solution - actual value
 residual = norm(A * x - B);
 disp(['Residual: ', num2str(residual)])
+disp("===================================================================") %Line for seperation    
 
-%PART B
-disp("Part B of Question 6.4")
 
-%Changing righ-hand side to 25.503 to 25.510
-while(B(2) < 25.510)
-    B(2) = B(2) + 0.001
-    residual = norm(A * x - B);
-    condition_estimator = rcond(A);
-    determinant_A = det(A);
-    disp(['Residual: ', num2str(residual)])
-    disp(['Condition Estimator (rcond): ', num2str(condition_estimator)])
-    disp(['Determinant of A (det): ', num2str(determinant_A)])
+%Testing 25.501
+disp("Testing 25.501:");
+B(2) = 25.501;
+x = A \ B;
+disp(['Solution for the system of A and B x = ', num2str(x(1)), ' y = ', num2str(x(2))]);
+residual = norm(A * x - B);
+disp(['Residual: ', num2str(residual)]);
+disp("===================================================================")   
+
+%Testing 25.502
+disp("Testing 25.502:");
+B(2) = 25.502;
+x = A \ B;
+disp(['Solution for the system of A and B x = ', num2str(x(1)), ' y = ', num2str(x(2))]);
+residual = norm(A * x - B);
+disp(['Residual: ', num2str(residual)]);
+disp("===================================================================")
+
+%Testing 25.504
+disp("Testing 25.504:");
+B(2) = 25.504;
+x = A \ B;
+disp(['Solution for the system of A and B x = ', num2str(x(1)), ' y = ', num2str(x(2))]);
+residual = norm(A * x - B);
+disp(['Residual: ', num2str(residual)]);
+disp("===================================================================")
+
+%Checking for ill conditioning
+determiant = det(A);
+condition = rcond(A);
+disp(['Condition: ', num2str(condition)]);
+disp(['Determiant: ', num2str(determiant)]);
+disp('Both rcond and det are extremely small or close to zero')
+disp('This is indicative of ill conditioning')
+disp("===================================================================")
+
+%Sensitivity Analysis. Testing with 1% change per iteration until 10%
+p = 0.01;
+while(p <= 0.1)
+    disp(["Testing with", num2str(p*100), "% change"]);
+    p_A = A + (A * p);
+    p_x = p_A\B;
+    disp(['Solution for the system of A and B x = ', num2str(p_x(1)), ' y = ', num2str(p_x(2))]);
+    residual = norm(p_A * p_x - B);
+    disp(['Residual: ', num2str(residual)]);
+    p = p + 0.01;
+    disp("===================================================================")
 end
+    
+
+
+
+
