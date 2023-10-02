0fd61b870553f2ef0e1bcea2f647ccae58ec37f3
Kymkim <82325590+Kymkim@users.noreply.github.com>
2023-10-01 22:30:45 -0700

    Finished Bisection Method

--- Assignment 3/bisectest.m
+++ Assignment 3/bisectest.m
@@ -1,3 +1,19 @@
+% Define the function
+f = @(x) 10*x^2 + x^3;
 
-[a,b,c] = bisec(1,1,1,1,1)
-disp(a)
+% Set the bounds and parameters
+a = -12;
+b = 0;
+tol = 1e-6;
+maxIter = 10;
+
+% Call the bisection method
+[root, err, x] = bisec(f, a, b, tol, maxIter);
+
+% Display the results
+disp('Root: ');
+disp(root);
+disp('Approximating errors: ');
+disp(err);
+disp('Apsproximating values of the root: ');
+disp(x);
