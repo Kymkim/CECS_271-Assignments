a4857ec27f40373c5129248659d41debef94d789
Kymkim <82325590+Kymkim@users.noreply.github.com>
2023-10-01 21:30:25 -0700

    bisec uncomplete

--- Assignment 3/bisec.m
+++ Assignment 3/bisec.m
@@ -4,9 +4,30 @@ Date: 10/01/2023
 Class: CECS 271
 Instructor: Minhthong Nguyen
 Purpose: USolves the root using bisection method
-Last updated: 10/01/2023
+Last updated: 10/01/2023asdqwe
 %}
 
 function [root, err, x] = bisec(f,a,b,tol,maxIter)
     
+    %Calculating for the first estimate
+    x = (b+a)/2;
+    err = b-f;
+    iter = 1
+
+    while or((abs(x(iter)) > tol), iter >= maxIter)
+        disp(iter)
+        if err(iter) > 0
+            b = x;
+        elseif err(iter) < 0
+            a = x;
+        else
+            break;
+        end
+        x = [x, (b+a)/2];
+        err = [err, b-f];
+        iter = iter + 1;
+    end
+
+    root = x(iter);
 end
+
