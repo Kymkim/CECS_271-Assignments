e84e1354d72d3203811fb554327da56127e77d3a
Kymkim <82325590+Kymkim@users.noreply.github.com>
2023-10-01 20:15:33 -0700

    Create bisec.m

---
+++ Assignment 3/bisec.m
@@ -0,0 +1,12 @@
+%{
+Name: Bisection Method
+Date: 10/01/2023
+Class: CECS 271
+Instructor: Minhthong Nguyen
+Purpose: USolves the root using bisection method
+Last updated: 10/01/2023
+%}
+
+function [root, err, x] = bisec(f,a,b,tol,maxIter)
+    
+end
