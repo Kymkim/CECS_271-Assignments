7298448e761d36953030eea20a710b0ed3522bf2
GittyGittyGat <94490996+GittyGittyGat@users.noreply.github.com>
2023-11-07 01:03:04 -0800

    update

--- ASsignment 4/Newton Method/newtonInter.m
+++ ASsignment 4/Newton Method/newtonInter.m
@@ -1,23 +1,23 @@
 function a = newtonInter(x, y)
-a = zeros(size(y));
+a = zeros(1,size(y,2));
 a(1) = y(1);
-delta = zeros(size(y)-1);
+delta = zeros(1,size(y,2)-1);
 
-Y_SIZE = size(y);
-for c = 1:(Y_SIZE(1,2)-1)
-    delta(1,c) = (y(1,c+1)-y(1,c))/(x(1,c+1)-x(1,c));
+for c = 2:(size(y,2))
+    delta(1,c-1) = (y(1,c)-y(1,c-1))./(x(1,c)-x(1,c-1));
 end
 
 a(1,2) = delta(1,1);
 X_SIZE = size(x);
 DE_SIZE = size(delta);
-
+temp = delta;
 for c = 3:X_SIZE(1,2)
 
     for s = (c-1):DE_SIZE(1,2)
-        delta(1,s) = (delta(1,s)-delta(1,(s-1)))/(x(1,s+1)-x(1,s-c+2));
-
+        delta(1,s) = (temp(1,s)-temp(1,(s-1)))/(x(1,s+1)-x(1,s-c+2));
     end
+    temp = delta;
     a(1,c) = delta(1,c-1);
 end
 end
+
